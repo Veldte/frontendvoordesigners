d3e7c2d9391120d9e0da5a5eb07b1eb9dc94e08b
Veldte <eva.veldt@hva.nl>
2019-03-24 16:57:35 +0100

    versie 1.2 opdracht 3 films

--- opdracht3/XMLHttpRequest kopie v1/script.js
+++ opdracht3/XMLHttpRequest kopie v1/script.js
@@ -61,10 +61,11 @@ function showData(jsonObj) {
 
         //functies om elementen toe te voegen
         reviewsbutton.info = reviewslezen; //zo kun je een variabele aanmaken in een object om iets te 'bewaren', bijvoorbeel een verwijzing naar een ander element
-        reviewsbutton.onclick = function () {
+        reviewsbutton.onclick = function details() {
             console.log("click this =" + this.info, this); //met this, verwijs je naar het object zelf waar de functie wordt uitgevoerd, hier gebruik ik de variabale die in in het object heb aangemaakt
             //console.log("parentNode",this.parentNode);
             this.info.classList.toggle('show')
+
         } //end: reviewsheader onClick
 
 
@@ -101,92 +102,93 @@ function checkKeyPress(key) {
     if (key.keyCode == "13") {
 
         loadimagesmetXHR();
-    }
+    } 
+    
 
-    //    if (key.keyCode == "39") {
-    //        
-    //        for (var i = 0; i < films.length; i++) {
-    //            loadimagesmetXHR9(films[i]);
-    //        }
-    //    }
-};
-
-
-//https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-//https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
-function loadimagesmetXHR() {
-    var request = new XMLHttpRequest();
-    request.open('get', uri);
-    request.responseType = 'json';
-    //request.responseType = 'text'; // now we're getting a string!
-    request.send();
-
-    request.addEventListener("load", function () {
-        //console.log("request load: ",request.response);
-        loaderElement.classList.remove('show');
-        console.log("XHR data", request.response);
-        console.table(request.response);
-        showData(request.response);
-    });
-    //  request.onload = function() {
-    //      console.log("request.onload: ",request.response);
-    //    }
-    /*request.timeout = 10000; // time in milliseconds
-      request.ontimeout = function(e) {
-        // XMLHttpRequest timed out. Do something here.
-        console.log("ontimeout: " +request.timeout+", het laden duurt te lang !",e);
-      };
-      */
-    request.onerror = function () {
-        console.log('Fetch Error', request.status);
+        //    if (key.keyCode == "39") {
+        //        
+        //        for (var i = 0; i < films.length; i++) {
+        //            loadimagesmetXHR9(films[i]);
+        //        }
+        //    }
     };
-}
-//loadimagesmetXHR();
 
 
-//actie
+    //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+    //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+    function loadimagesmetXHR() {
+        var request = new XMLHttpRequest();
+        request.open('get', uri);
+        request.responseType = 'json';
+        //request.responseType = 'text'; // now we're getting a string!
+        request.send();
 
-
-button.onclick = function () {
-    loaderElement.classList.add('show');
-    //this.classList.add('hide');
-    //hier iets doen met de button die laadt, dan weer gewoon een button wordt ...
-
-    section.innerHTML = ""; //main leeghalen. just in case
-    loadimagesmetXHR();
-};
-
-
-
-
-
-
-function loadRestApiFetch() { //Rest Api call met Fetchs
-    console.log("function loadRestApiFetch");
-
-    loaderElement.classList.add('show');
-    fetch(uri)
-        .then(function (response) {
-            console.log(response.headers.get('Content-Type'));
-            console.log(response.headers.get('Date'));
-
-            console.log(response.status);
-            console.log(response.statusText);
-            console.log(response.type);
-            console.log(response.url);
-
-            return response.json();
-        })
-        .then(function (myJson) {
-            console.log('Request successful', myJson);
-            //eerst de loader weg halen !
+        request.addEventListener("load", function () {
+            //console.log("request load: ",request.response);
             loaderElement.classList.remove('show');
-            //dan de html renderen
-            //document.querySelector("p").innerHTML="joehoe";
-            //console.log(myJson);
-        })
-        .catch(function (error) {
-            console.log('Request failed', error)
+            console.log("XHR data", request.response);
+            console.table(request.response);
+            showData(request.response);
         });
-}
-//loadRestApiFetch();
+        //  request.onload = function() {
+        //      console.log("request.onload: ",request.response);
+        //    }
+        /*request.timeout = 10000; // time in milliseconds
+          request.ontimeout = function(e) {
+            // XMLHttpRequest timed out. Do something here.
+            console.log("ontimeout: " +request.timeout+", het laden duurt te lang !",e);
+          };
+          */
+        request.onerror = function () {
+            console.log('Fetch Error', request.status);
+        };
+    }
+    //loadimagesmetXHR();
+
+
+    //actie
+
+
+    button.onclick = function () {
+        loaderElement.classList.add('show');
+        //this.classList.add('hide');
+        //hier iets doen met de button die laadt, dan weer gewoon een button wordt ...
+
+        section.innerHTML = ""; //main leeghalen. just in case
+        loadimagesmetXHR();
+    };
+
+
+
+
+
+
+    function loadRestApiFetch() { //Rest Api call met Fetchs
+        console.log("function loadRestApiFetch");
+
+        loaderElement.classList.add('show');
+        fetch(uri)
+            .then(function (response) {
+                console.log(response.headers.get('Content-Type'));
+                console.log(response.headers.get('Date'));
+
+                console.log(response.status);
+                console.log(response.statusText);
+                console.log(response.type);
+                console.log(response.url);
+
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log('Request successful', myJson);
+                //eerst de loader weg halen !
+                loaderElement.classList.remove('show');
+                //dan de html renderen
+                //document.querySelector("p").innerHTML="joehoe";
+                //console.log(myJson);
+            })
+            .catch(function (error) {
+                console.log('Request failed', error)
+            });
+    }
+    //loadRestApiFetch();
